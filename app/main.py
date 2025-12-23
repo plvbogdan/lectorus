210602f8d13217d205ec691d92c7d9e75655a14d
plvbogdan <plvbogdan@gmail.com>
2025-12-23 21:37:35 +0300

    feat/ cors

--- app/main.py
+++ app/main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
 from app.routers import lectures
 from app.database import Base, engine
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI(
     title="Лекторус",
     version="0.1.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(lectures.router)
